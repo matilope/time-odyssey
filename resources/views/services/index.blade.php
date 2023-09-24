5a489d41cbae8133358d2107a877c5e550519a5b
lclob <116601056+lclob@users.noreply.github.com>
2023-09-23 21:25:03 -0300

    probando

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -4,4 +4,5 @@
 
 @section('content')
 <h1>Time Odyssey</h1>
+<p>Time Odyssey is a game that is currently in development. It is a game that is based on the concept of time travel. The game is currently in development and will be released in the future.</p>
 @endsection
